a3f79cacd526f03c344a7aadb16debd5237644f5
Humberto Tellechea <humberto.htm@gmail.com>
2020-12-02 22:10:57 -0700

    Now parsing flags correctly

--- main.go
+++ main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
-	// Incorporate logging utility instead of just printing lines
+	// TODO: Incorporate logging utility instead of just printing lines
 	fmt.Println("Starting screener..")
 
 	coDetailsCmd := flag.NewFlagSet("companydetails", flag.ExitOnError)
 	coCIK := coDetailsCmd.String("cik", "", "cik")
+	coDetailsCmd.Usage = func() {
+		fmt.Println("cik flag not provided")
+		fmt.Println("Usage: -cik string")
+	}
 
 	if len(os.Args) < 2 {
 		fmt.Println("Expected fullscreen or companydetails subcommands")
@@ -24,8 +28,14 @@ func main() {
 		fmt.Println("Executing full screening task...")
 	// go run main.go companydetails --cik=111111
 	case "companydetails":
-		coDetailsCmd.Parse(os.Args[2:])
-		fmt.Printf("Extracting details for CIK %s\n", *coCIK)
+		if len(os.Args[2:]) < 1 {
+			coDetailsCmd.Usage()
+
+		} else {
+			coDetailsCmd.Parse(os.Args[2:])
+			fmt.Printf("Extracting details for CIK %s\n", *coCIK)
+		}
+
 	default:
 		fmt.Println("Expected fullscreen or companydetails subcommands")
 		os.Exit(1)

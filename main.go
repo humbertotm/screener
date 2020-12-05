f4d83b35224fa5bbd6071c6707dca749750cc8db
Humberto Tellechea <humberto.htm@gmail.com>
2020-12-05 14:53:15 -0700

    Now establishing connection with MongoDB

--- main.go
+++ main.go
@@ -1,15 +1,63 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
 	// TODO: Incorporate logging utility instead of just printing lines
 	fmt.Println("Starting screener..")
 
+	// Initiate context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Initiate MongoDB connection
+	client, err := initMongoClient(ctx)
+	if err != nil {
+		fmt.Printf("Error while establishing connection with MongoDB: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	// Terminate MongoDB connection
+	// TODO: gracefully handle this panic?
+	defer func() {
+		fmt.Println("Terminating mongodb connection")
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Handle input subcommand
+	handleCommand(client)
+
+	// Exit program successfully
+	os.Exit(0)
+}
+
+func initMongoClient(ctx context.Context) (*mongo.Client, error) {
+	clientOpts := options.Client()
+	clientOpts.ApplyURI("mongodb://localhost:27017")
+	clientOpts.SetAuth(options.Credential{
+		Username: "mongoadmin",
+		Password: "mongoadmin",
+	})
+	client, err := mongo.Connect(ctx, clientOpts)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+// TODO: return errors and let main function handle the exit
+func handleCommand(client *mongo.Client) {
 	coDetailsCmd := flag.NewFlagSet("companydetails", flag.ExitOnError)
 	coCIK := coDetailsCmd.String("cik", "", "cik")
 	coDetailsCmd.Usage = func() {
@@ -18,6 +66,7 @@ func main() {
 	}
 
 	if len(os.Args) < 2 {
+		// A more apprpriate Usage handling function call would be better here
 		fmt.Println("Expected fullscreen or companydetails subcommands")
 		os.Exit(1)
 	}
@@ -30,6 +79,7 @@ func main() {
 	case "companydetails":
 		if len(os.Args[2:]) < 1 {
 			coDetailsCmd.Usage()
+			os.Exit(1)
 
 		} else {
 			coDetailsCmd.Parse(os.Args[2:])
@@ -40,6 +90,5 @@ func main() {
 		fmt.Println("Expected fullscreen or companydetails subcommands")
 		os.Exit(1)
 	}
-
-	os.Exit(0)
+	return
 }
